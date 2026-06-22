269fbe0cf18fa098f91f09ad751ba7de0e876f5b
Yuri Zinchenko <zinchen777@gmail.com>
2026-06-22 10:30:56 +0000

    22 июн. 2026 г., 13:30

--- app/spec/support/capybara.rb
+++ app/spec/support/capybara.rb
@@ -2,7 +2,7 @@ require "capybara/rails"
 require "capybara/cuprite"
 
 Capybara.register_driver(:cuprite) do |app|
-  Capybara::Cuprite::Driver.new(app, window_size: [ 1280, 800 ], headless: true)
+  Capybara::Cuprite::Driver.new(app, window_size: [ 1280, 800 ], headless: true, process_timeout: 30)
 end
 
 Capybara.configure do |config|
